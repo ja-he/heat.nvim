5c1c08703b619962fb42d1282544d4249c211f9f
Jan Hensel <ja_he@uni-bremen.de>
2023-05-03 11:55:48 +0200

    Add a timestamp finder draft

--- lua/fugitive-blame-heatmap/init.lua
+++ lua/fugitive-blame-heatmap/init.lua
@@ -3,6 +3,17 @@
 
 -- core plugin logic
 
+local function get_timestamps_in_blame(git_blame_data)
+  local result = {}
+  for _, line in ipairs(git_blame_data) do
+    local timestamp_start, timestamp_end, timestamp = line:find("(%d%d%d%d%-%d%d%-%d%d %d%d:%d%d:%d%d [+-]%d%d%d%d)")
+    if timestamp_start then
+    end
+  end
+  return result
+end
+
 return {
+  get_timestamps_in_blame = get_timestamps_in_blame,
   dummy_func = function() print("fugitive-blame-heatmap 😎") end,
 }
