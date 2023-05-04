20b0101afe60184cb5c5b81530f0ca55dd20a45c
Jan Hensel <ja_he@uni-bremen.de>
2023-05-04 15:40:28 +0200

    Automatically attach to 'fugitiveblame'-ft bufs

--- plugin/fugitive-blame-heatmap.lua
+++ plugin/fugitive-blame-heatmap.lua
@@ -2,6 +2,14 @@
 -- SPDX-License-Identifier: MIT
 
 -- basic plugin infrastructure
+local augroup = vim.api.nvim_create_augroup("fugitive-blame-heatmap", {clear = true})
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup,
+  pattern = 'fugitiveblame',
+  callback = function()
+    require('fugitive-blame-heatmap').get_timestamps_in_blame()
+  end,
+})
 vim.api.nvim_create_user_command(
   "FugitiveBlameHeatmap",
   require('fugitive-blame-heatmap').get_timestamps_in_blame,
